bdad62e16f610332a2b10dd7e576415d0cdbc390
Ben Sims <arycama@gmail.com>
2026-07-16 11:47:07 +0930

    Light loop code tidying

--- Assets/Shaders/Common.hlsl
+++ Assets/Shaders/Common.hlsl
@@ -92,21 +92,21 @@ SamplerComparisonState LinearClampCompareSampler;
 		unity_Builtins0Array[2];
 	};
 #else
-	cbuffer UnityPerDraw
-	{
-		float3x4 unity_ObjectToWorld, unity_WorldToObject;
-		float4 unity_LODFade; // x is the fade value ranging within [0,1]. y is x quantized into 16 levels
-		float4 unity_WorldTransformParams; // w is usually 1.0, or -1.0 for odd-negative scale transforms
-	};
+cbuffer UnityPerDraw
+{
+	float3x4 unity_ObjectToWorld, unity_WorldToObject;
+	float4 unity_LODFade; // x is the fade value ranging within [0,1]. y is x quantized into 16 levels
+	float4 unity_WorldTransformParams; // w is usually 1.0, or -1.0 for odd-negative scale transforms
+};
 #endif
 
 float3 ObjectToWorld(float3 position, uint instanceId)
 {
-	#ifdef INSTANCING_ON
+#ifdef INSTANCING_ON
 		float3x4 objectToWorld = (float3x4)unity_Builtins0Array[unity_BaseInstanceID + instanceId].unity_ObjectToWorldArray;
-	#else
-		float3x4 objectToWorld = unity_ObjectToWorld;
-	#endif
+#else
+	float3x4 objectToWorld = unity_ObjectToWorld;
+#endif
 	
 	return MultiplyPoint3x4(objectToWorld, position);
 }
@@ -123,11 +123,11 @@ float3 ObjectToViewPosition(float3 position, uint instanceId)
 
 float4 WorldToClipPosition(float3 position)
 {
-	#ifdef UNITY_PASS_SHADOWCASTER
+#ifdef UNITY_PASS_SHADOWCASTER
 		return MultiplyPoint(WorldToShadowClip, position);
-	#else
-		return MultiplyPoint(WorldToClip, position);
-	#endif
+#else
+	return MultiplyPoint(WorldToClip, position);
+#endif
 }
 
 float4 ObjectToClip(float3 position, uint instanceId)
@@ -138,11 +138,11 @@ float4 ObjectToClip(float3 position, uint instanceId)
 float3 ObjectToWorldNormal(float3 normal, uint instanceId)
 {
 	// Source: https://www.shadertoy.com/view/3s33zj
-	#ifdef INSTANCING_ON
+#ifdef INSTANCING_ON
 		float3x4 objectToWorld = (float3x4)unity_Builtins0Array[unity_BaseInstanceID + instanceId].unity_ObjectToWorldArray;
-	#else
-		float3x4 objectToWorld = unity_ObjectToWorld;
-	#endif
+#else
+	float3x4 objectToWorld = unity_ObjectToWorld;
+#endif
 	
 	float3x3 adjoint = float3x3(cross(objectToWorld[1].xyz, objectToWorld[2].xyz), cross(objectToWorld[2].xyz, objectToWorld[0].xyz), cross(objectToWorld[0].xyz, objectToWorld[1].xyz));
 	return normalize(mul(adjoint, normal));
@@ -167,38 +167,41 @@ float3 GetLighting(float3 normal, float3 worldPosition, float4 screenPosition)
 	if (NdotL && fade && all(saturate(shadowPosition.xy) == shadowPosition.xy))
 		lighting *= lerp(1.0, SunShadow.SampleCmpLevelZero(LinearClampCompareSampler, shadowPosition.xy, shadowPosition.z), fade);
 	
-	// Point Lights
 	uint lightCount = 0;
-	uint2 clusterCoord = (uint2) (screenPosition.xy / TileSize);
-	uint tileDepth = (uint) (log2(screenPosition.w) * LinearToLogScale + LinearToLogOffset);
 	
-	uint linearClusterCoord = clusterCoord.y * TileCountX + clusterCoord.x;
+	// Point Lights
+	// Flat bit array iterator scalarized on entity with Z-Bin masked words
+	uint3 cluster = float3(screenPosition.xy / TileSize, log2(screenPosition.w) * LinearToLogScale + LinearToLogOffset);
+	uint2 depthRanges = BitUnpack(LightDepthMinMax[cluster.z], 16, uint2(0, 16));
 	
-	uint2 zRange = BitUnpack(LightDepthMinMax[tileDepth], 16, uint2(0, 16));
+	uint start = WaveReadLaneFirst(WaveActiveMin(depthRanges.x)) >> 5u; // mergedMin scalar from this point
+	uint end = WaveReadLaneFirst(WaveActiveMax(depthRanges.y)) >> 5u; // mergedMax scalar from this point
 	
-	// Find min/max light we need to consider when shading slice Z
-	uint zStart = WaveActiveMin(zRange.x) >> 5u;
-	uint zEnd = WaveActiveMax(zRange.y) >> 5u;
+	uint tileIndex = (cluster.y * TileCountX + cluster.x) * LightIndexCount;
 	
-	for (uint i = zStart; i <= zEnd; i++)
+	// Read range of words of visibility bits
+	for (uint i = start; i <= end; i++)
 	{
-		uint mask = VisibleLightBits[linearClusterCoord * LightIndexCount + i];
+		// Load bit mask data per lane
+		uint tileMask = VisibleLightBits[tileIndex + i];
 		
-		// Restrict to lights within Z-range
-		uint startIndex = 32u * i;
+		// Mask by zbin mask
+		uint localMin = clamp((int) depthRanges.x - (int) (32 * i), 0, 31);
+		uint maskWidth = clamp((int) depthRanges.y - (int) depthRanges.x + 1, 0, 32);
 		
-		uint2 range;
-		range.x = clamp(zRange.x, startIndex, startIndex + 32u);
-		range.y = clamp(zRange.y + 1u, range.x, startIndex + 32u);
-
-		uint numBits = range.y - range.x;
-		uint rangeMask = numBits == 32u ? UintMax : ((1u << numBits) - 1u) << (range.x - startIndex);
-		mask = WaveActiveBitOr(mask & rangeMask);
-	
+		// BitFieldMask op needs manual 32 size wrap support
+		uint depthMask = maskWidth == 32u ? UintMax : ((1u << maskWidth) - 1u) << localMin;
+		
+		// Compact world bitmask over all lanes in wavefront
+		uint mask = WaveReadLaneFirst(WaveActiveBitOr(tileMask & depthMask));
 		while (mask != 0u)
 		{
 			uint bitIndex = firstbitlow(mask);
-			Light light = PointLights[startIndex + bitIndex];
+			uint lightIndex = 32u * i + bitIndex;
+			mask &= ~(1u << bitIndex);
+			lightCount++;
+			
+			Light light = PointLights[lightIndex];
 	
 			// Attenuation
 			float3 lightVector = light.position - worldPosition;
@@ -206,16 +209,14 @@ float3 GetLighting(float3 normal, float3 worldPosition, float4 screenPosition)
 			float attenuation = saturate(1.0h - distanceSquared * light.rcpRangeSquared);
 		
 			float3 L = lightVector * rsqrt(distanceSquared);
-			attenuation = Sq(attenuation * saturate(dot(light.forward, L) * light.angleScale + light.angleOffset));
-		
-			lighting += saturate(dot(normal, L)) * light.color * attenuation;
-			mask &= ~(1u << bitIndex);
+			attenuation *= saturate(dot(light.forward, L) * light.angleScale + light.angleOffset);
+			attenuation = Sq(attenuation);
 			
-			lightCount++;
+			lighting += saturate(dot(normal, L)) * attenuation * light.color;
 		}
 	}
 	
-	//lighting = lightCount >> uint3(0, 1, 2) & 1;
+	//lighting = (lightCount >> uint3(0, 1, 2) & 1);
 	
 	return lighting;
 }
